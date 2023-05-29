9ea896c4a4ec528d98b62a75730090911c48f0c4
santosh <santoshtechguy@gmail.com>
2023-05-29 17:45:02 +0300

    keypair modules created

--- dev_workspace/variables.tf
+++ dev_workspace/variables.tf
@@ -4,3 +4,7 @@ variable "vpc_cidr" {}
 variable "public_subnet_az1_cidr" {}
 variable "private_subnet_az1_cidr" {}
 variable "instance_type" {}
+variable "keypair_algorithm" {}
+variable "keypair_name" {}
+variable "rsa_bit" {}
+  
