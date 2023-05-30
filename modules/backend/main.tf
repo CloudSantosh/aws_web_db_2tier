cd56385611c8feb9f9e532e1853b9ed08c4cca30
santosh <santoshtechguy@gmail.com>
2023-05-30 22:43:52 +0300

    some changes made on backend

--- modules/backend/main.tf
+++ modules/backend/main.tf
@@ -5,13 +5,14 @@ resource "aws_db_subnet_group" "db_subnet_group" {
 }
 
 resource "aws_db_instance" "rds_server_backend" {
-  allocated_storage      = 10
-  db_name                = "awsrestartdb123"
+  allocated_storage = 10
+  //db_name                = "awsrestartdb123"
   engine                 = "mysql"
+  identifier             = "rds_database"
   engine_version         = "5.7"
   instance_class         = "db.t2.micro"
-  username               = "awsrestart"
-  password               = "Awsrestart123"
+  username               = "admin"
+  password               = "Password"
   parameter_group_name   = "default.mysql5.7"
   db_subnet_group_name   = aws_db_subnet_group.db_subnet_group.name
   vpc_security_group_ids = [var.private_rds_security_group_id]
